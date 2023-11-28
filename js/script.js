c43f4675f311a5d390ab3c4cb102076be7558490
LeandroDukievicz <ldukie@icloud.com>
2023-11-27 22:53:51 -0300

    FEAT - Concluded First section apresentation

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const texto = "Seja bem vindo(a) ao Portifólio de : ";
+      const tempoDigitacao = 150; // Tempo em milissegundos
+      const tempoPausa = 1000; // Tempo de pausa em milissegundos entre loops
+    
+      function maquinaEscrever(texto, elemento, tempo) {
+        let i = 0;
+        function escrever() {
+          elemento.innerHTML += texto.charAt(i);
+          i++;
+          if (i <= texto.length) {
+            setTimeout(escrever, tempo);
+          } else {
+            setTimeout(resetar, tempoPausa);
+          }
+        }
+    
+        function resetar() {
+          i = 0;
+          elemento.innerHTML = '';
+          setTimeout(escrever, tempoPausa);
+        }
+    
+        escrever();
+      }
+    
+      window.onload = function() {
+        const divTexto = document.getElementById('maquina-escrever');
+        maquinaEscrever(texto, divTexto, tempoDigitacao);
+      };
